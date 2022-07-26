2ae74b1626f42168dd544ec0dd43aed22be65393
cjslack <40322960+cjslack@users.noreply.github.com>
2022-07-26 16:38:31 -0400

    updated publisher id

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -1,21 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 export const Ad = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
 
-    useEffect(() => {
-        const script = document.createElement('script');
+    script.src = "https://media.ethicalads.io/media/client/ethicalads.min.js";
+    script.async = true;
 
-        script.src = "https://media.ethicalads.io/media/client/ethicalads.min.js";
-        script.async = true;
-      
-        document.body.appendChild(script);
-      
-        return () => {
-          document.body.removeChild(script);
-        }
-      }, []);
+    document.body.appendChild(script);
 
-    return (
-        <div className="dark bordered" data-ea-publisher="httpsgrokdebuggercom" data-ea-type="image"></div>
-    )
-}
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return <div className="dark bordered" data-ea-publisher="grokdebugger-com" data-ea-type="image"></div>;
+};
